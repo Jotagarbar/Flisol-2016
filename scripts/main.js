9b9be73b043647eb92d07df9bc8169aa1d92f16f
Erick Carvajal R <neocarvajal12@gmail.com>
2016-04-04 06:47:52 -0430

    testing template

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("Fisol Cumana 2016 UP");
+
+/*! Main */
+jQuery(document).ready(function($) {
+
+   //  navbar fixed saliendo del banner
+    var navbar = $('#navbar-main'),
+            distance = navbar.offset().top,
+        $window = $(window);
+
+    $window.scroll(function() {
+        if ($window.scrollTop() >= distance) {
+            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+            $("body").css("padding-top", "60px");
+        } else {
+            navbar.removeClass('navbar-fixed-top');
+            $("body").css("padding-top", "0px");
+        }
+    });
+});
